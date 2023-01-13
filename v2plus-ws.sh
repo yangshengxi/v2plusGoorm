8528dd1ecb8e7727bdc1e7f276627d0d949aba4c
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-13 11:29:43 +0800

    Update v2plus-ws.sh

--- v2plus-ws.sh
+++ v2plus-ws.sh
@@ -5,6 +5,6 @@
 wget -q -O main https://github.com/ShadowObj/v2plusGoorm/raw/main/main
 sudo echo "helloworld" > /home/helloworld.txt
 chmod +x ./main
-echo '{"log":{"access":"","error":"","loglevel":"warning"},"inbound":{"protocol":"vmess","port":10001,"settings":{"clients":[{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","alterId":0 }]},"streamSettings":{"network":"ws","wsSettings":{"path":"/zdx520/forever"}}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' > ./config.json
+echo '{"log":{"access":"","error":"","loglevel":"warning"},"inbound":{"protocol":"vmess","port":80,"settings":{"clients":[{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","alterId":0 }]},"streamSettings":{"network":"ws","wsSettings":{"path":"/zdx520/forever"}}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' > ./config.json
 nohup ./main run > /dev/null 2>&1 &
 sudo rm /home/helloworld.txt ./main
