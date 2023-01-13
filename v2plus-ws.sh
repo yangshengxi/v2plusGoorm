fc9dd3256c61b183cf3804f7fa21084d55e760ba
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-13 11:29:29 +0800

    Update v2plus-ws.sh

--- v2plus-ws.sh
+++ v2plus-ws.sh
@@ -4,7 +4,7 @@
 # 仅简单修改main包main.go入口代码
 wget -q -O main https://github.com/ShadowObj/v2plusGoorm/raw/main/main
 sudo echo "helloworld" > /home/helloworld.txt
-sudo chmod +x ./main
+chmod +x ./main
 echo '{"log":{"access":"","error":"","loglevel":"warning"},"inbound":{"protocol":"vmess","port":10001,"settings":{"clients":[{"id":"ffffffff-ffff-ffff-ffff-ffffffffffff","alterId":0 }]},"streamSettings":{"network":"ws","wsSettings":{"path":"/zdx520/forever"}}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' > ./config.json
 nohup ./main run > /dev/null 2>&1 &
 sudo rm /home/helloworld.txt ./main
