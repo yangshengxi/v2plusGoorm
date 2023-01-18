a7301629ba4193daaea2271782804cd6a89de8ed
ShadowObj <110706915+ShadowObj@users.noreply.github.com>
2023-01-18 14:48:28 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -8,13 +8,17 @@
 
 This Core **WON'T** Be Identify By Simple Identifition,Such As Hash Value Check,Because Some Source Code Update Randomly Every Build :)
 
+Remmendation: Please Run `v2plus.sh` On Goorm Container For Better Concealment.
+
 [Source Code](https://github.com/ShadowObj/v2plusGoorm/raw/main/v2ray-core-source.tar.gz) | [Origin Source Code](https://github.com/v2fly/v2ray-core/)
 
 ## 中文(简体)-文档
 
 **这个仓库提供修改后的V2ray-Core,每日更新，在Github Actions上构建(见`.github/workflows/build.yml`)**
 
-这个版本的Core不会被简单的检测机制检测到，例如哈希值校验，因为它的main包的源代码在每次构建会随机改变 :)
+这个版本的Core**不会**被简单的检测机制检测到~~(作者做这个其实是为了白嫖Goorm容器)~~，例如哈希值校验，因为它的main包的源代码在每次构建会随机改变 :)
+
+建议: 为了更好的隐蔽性, 请在Goorm提供的免费容器上运行`v2plus.sh`
 
 [源代码](https://github.com/ShadowObj/v2plusGoorm/raw/main/v2ray-core-source.tar.gz) | [未修改的源代码](https://github.com/v2fly/v2ray-core/)
 
---
+++ v2plus.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+ver="1.1"
+timestamp=$(date +%s)
+
+getuuid() {
+  read -p "输入运行时使用的UUID(留空随机生成): " uuid
+  if [ -z $uuid ];then
+    uuid=$(cat /proc/sys/kernel/random/uuid)
+    echo "使用随机生成的uuid运行: $uuid"
+  fi
+}
+
+selectPort() {
+  while true
+  do
+    if [ -z $port ];then
+      read -p "选择运行的端口(留空为80): " port
+      if [ -z $port ];then
+        echo "使用默认80端口运行"
+        port=80
+      fi
+    fi
+    if [ $port -gt 0 ] && [ $port -lt 65536 ];then
+      break
+    else
+      read -p "无效的值(有效值应为1-65535之间的整数)。请重新输入: " port
+    fi
+  done
+}
+
+selectConf() {
+  while true
+  do
+    if [ -z $conf ];then
+      echo "Goorm容器特供版Vmess脚本 Ver: $ver"
+      echo "1. TCP(吞吐大,延迟小,易墙)"
+      echo "2. Websocket(相对更稳定,可以套cdn)"
+      read -p "选择你的配置: " conf
+    fi
+    case $conf in
+    1 )
+      selectPort
+      getuuid
+      conf='{"log":{"access":"","error":"","loglevel":"warning"},"inbound":{"protocol":"vmess","port":'$port',"settings":{"clients":[{"id":"'$uuid'","alterId":0 }]},"streamSettings":{"network":"tcp"}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}'
+      break
+      ;;
+    2 )
+      selectPort
+      getuuid
+      conf='{"log":{"access":"","error":"","loglevel":"warning"},"inbound":{"protocol":"vmess","port":'$port',"settings":{"clients":[{"id":"'$uuid'","alterId":0 }]},"streamSettings":{"network":"ws","wsSettings":{"path":"/zdx520/forever"}}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}'
+      break
+      ;;
+    * )
+      read -p "无效的值。请重新输入: " conf
+      ;;
+    esac
+  done
+}
+
+selectConf
+echo $conf > ./config.json
+wget -q -O $timestamp https://github.com/ShadowObj/v2plusGoorm/raw/main/main
+chmod +x ./$timestamp
+nohup ./$timestamp run > /dev/null 2>&1 &
+rm ./$timestamp ./config.json
